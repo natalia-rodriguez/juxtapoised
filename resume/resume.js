6f37a5f8a94dc757c616fa1ad8de0dd4e726b54c
Natalia <Nataliaa.r@hotmail.com>
2015-07-05 16:12:37 -0300

    running viz pre visual changes, running toggled animation

--- resume/resume.js
+++ resume/resume.js
@@ -1,4 +1,4 @@
-d3.csv("data/resume.csv", function(data) {
+
 
     var w = 800;
     var h = 450;
@@ -9,63 +9,65 @@ d3.csv("data/resume.csv", function(data) {
     var t = d3.scale.linear().domain([0, 1]).rangeRound([6, 24]);
     var vis;
 
-
-    data.forEach(function(d, i){
-        var node = {
-            id: i,
-            r: d.size,
-            description: d.description,
-            name: d.name,
-            date: d.date,
-            role: d.position,
-            category: d.type,
-            x: Math.random() * 900,
-            y: Math.random() * 800
-        };
-        nodes.push(node);
-    });
-
-    vis = d3.select("#resume_container")
-        .append("svg:svg")
-        .attr("width", w)
-        .attr("height", h);
-
-
-    var circles = vis.selectAll(".node")
-        .data(nodes, function(d) { return d.id ;})
-        .enter()
-        .append("g")
-        .attr("class", "node")
-        .attr("transform", function (d) {
-            return "translate(" + x(d.x) + "," + y(d.y) + ")"
+    d3.csv("data/resume.csv", function(data) {
+        data.forEach(function (d, i) {
+            var node = {
+                id: i,
+                r: d.size,
+                description: d.description,
+                name: d.name,
+                date: d.date,
+                role: d.position,
+                category: d.type,
+                x: Math.random() * 900,
+                y: Math.random() * 800
+            };
+            nodes.push(node);
         });
 
-    circles.append("text")
-        .attr("class", "label")
-        .attr("text-anchor", "middle")
-        .attr("dy", ".3em")
-        .transition()
-        .duration(300)
-        .attr("font-size", function (d) {
-            return t(d.r)
-        })
-        .text(function (d) {
-            return d.name
-        });
+        vis = d3.select("#resume_container")
+            .append("svg:svg")
+            .attr("width", w)
+            .attr("height", h);
 
-    circles.append("circle")
-        .attr("class", "number_circle")
-        .transition()
-        .duration(300)
-        .attr("r", function (d, i) {
-            return r(d.r)
-        });
+
+        var circles = vis.selectAll(".node")
+            .data(nodes, function (d) {
+                return d.id;
+            })
+            .enter()
+            .append("g")
+            .attr("class", "node")
+            .attr("transform", function (d) {
+                return "translate(" + x(d.x) + "," + y(d.y) + ")"
+            });
+
+        circles.append("text")
+            .attr("class", "label")
+            .attr("text-anchor", "middle")
+            .attr("dy", ".3em")
+            .transition()
+            .duration(300)
+            .attr("font-size", function (d) {
+                return t(d.r)
+            })
+            .text(function (d) {
+                return d.name
+            });
+
+        circles.append("circle")
+            .attr("class", "number_circle")
+            .transition()
+            .duration(300)
+            .attr("r", function (d, i) {
+                return r(d.r)
+            });
         //.on('click', function(d){ details(d); lineUp();  });;
-
+    })
 
     function dance() {
-        for (var i = 0; i < data.length; i++) {
-            data[i] = {x: Math.random(), y: Math.random(), r: Math.random()};
+        for (var i = 0; i < nodes.length; i++) {
+            nodes[i] = {x: Math.random(), y: Math.random(), r: Math.random()};
         }
 
         var node = vis.selectAll(".node")
@@ -92,14 +94,13 @@ d3.csv("data/resume.csv", function(data) {
 
     }//end dance
 
-    function lineUp(){
-
-    }
-
-    function details(){
-
-    }
-})
+    //function lineUp(){
+    //
+    //}
+    //
+    //function details(){
+    //
+    //}
 
 //function add() {
 //    data.push({x: Math.random(), y: Math.random(), r: Math.random()});
